a44de9af335380afb71d29878fc6df3828d3bbbb
reihned <reihned@gmail.com>
2015-01-06 10:14:58 -0500

    sendgrid...

--- lib/connection.rb
+++ lib/connection.rb
@@ -1,6 +1,9 @@
 require 'active_record'
 require 'sendgrid-ruby'
+require 'json'
 
 ActiveRecord::Base.establish_connection('postgresql://' + ENV["DB_INFO"] + '@127.0.0.1/tastings')
-
-sg_client = SendGrid::Client.new(ENV["SG_INFO"])
+sg_info = JSON.parse(ENV["SG_INFO"])
+sg_user = sg_info["api_user"]
+sg_key = sg_info["api_key"]
+sg_client = SendGrid::Client.new(api_user: sg_user, api_key: sg_key)
