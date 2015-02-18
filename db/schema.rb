c524b2c1b89171d2280f2f1499f5ab2e62f8dc50
reihned <reihned@gmail.com>
2015-02-18 16:06:33 -0500

    migrations and models for reviews and corresponding votes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217163223) do
+ActiveRecord::Schema.define(version: 20150218204707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20150217163223) do
     t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "product_id"
+    t.text    "content"
+    t.boolean "positive"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,5 +49,10 @@ ActiveRecord::Schema.define(version: 20150217163223) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "review_id"
+    t.boolean "positive"
+  end
 
 end
