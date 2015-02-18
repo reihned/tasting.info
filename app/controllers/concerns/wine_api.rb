235ef960b6bf8bcb43c350dc316007a17cf7afa3
reihned <reihned@gmail.com>
2015-02-18 10:58:59 -0500

    got wine api working, it's always the smallest things

--- app/controllers/concerns/wine_api.rb
+++ app/controllers/concerns/wine_api.rb
@@ -12,14 +12,14 @@ module WineApi
     def search(search_raw)
       # sanitize the search string
       search_string = search_raw.scan(/[\s\w]/).join("")
-      search_string = search_string.gsub(" ", "%20")
+      # search_string = search_string.gsub(" ", "%20")
 
       # add the search string
       query = @options
       query[:query].merge!({"search" => search_string})
 
       # API request, execting json response
-      response = self.class.get("/beta2/service.svc/JSON/catalog", query)
+      response = self.class.get("/api/beta2/service.svc/JSON/catalog", query)
 
       # Only return what's needed, an array of results
       return response["Products"]["List"]
@@ -31,7 +31,7 @@ module WineApi
       query[:query] << {"product" => product_id}
 
       # API request, execting json response
-      self.class.get("/beta2/service.svc/JSON/catalog", query)
+      self.class.get("/api/beta2/service.svc/JSON/catalog", query)
     end
   end
 end
